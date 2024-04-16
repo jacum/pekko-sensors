e3bced2d3a4fb84184f75dca1fe91802a5b09432
Tim Evdokimov <tim@pragmasoft.nl>
2024-04-16 16:46:57 +0200

    azure build and releases

--- project/Publish.scala
+++ project/Publish.scala
@@ -51,7 +51,8 @@ object Publish {
           checkSnapshotDependencies,
           inquireVersions,
           runClean,
-//      runTest, // can't run test w/cross-version release
+          setReleaseVersion,
+      //      runTest, // can't run test w/cross-version release
           releaseStepCommandAndRemaining("+publishSigned"),
           releaseStepCommand("sonatypeBundleRelease")
         )
