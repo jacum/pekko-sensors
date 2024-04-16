8e020ff8344f49a09df35160686ad07828f0e254
Tim Evdokimov <tim@pragmasoft.nl>
2024-04-16 15:27:55 +0200

    added inmem journal

---
+++ pekko-inmem-journal/src/test/scala/org/apache/pekko/persistence/inmemory/query/ConcurrentUUIDTest.scala
@@ -0,0 +1,15 @@
+package org.apache.pekko.persistence.inmemory.query
+
+import org.apache.pekko.persistence.inmemory.TestSpec
+import org.scalatest.Ignore
+
+import java.util.UUID
+import scala.concurrent.Future
+
+@Ignore
+class ConcurrentUUIDTest extends TestSpec {
+  def getNow(x: Any): Future[UUID] = Future(org.apache.pekko.persistence.inmemory.nowUuid)
+  it should "get uuids concurrently" in {
+    Future.sequence((1 to 1000).map(getNow)).futureValue
+  }
+}
