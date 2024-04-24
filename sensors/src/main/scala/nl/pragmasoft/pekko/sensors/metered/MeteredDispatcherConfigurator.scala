ace2555210b989fc5b6c61910e102f186ac974af
PJ Fanning <pjfanning@users.noreply.github.com>
2024-04-24 03:51:09 +0200

    broken tests

--- sensors/src/main/scala/nl/pragmasoft/pekko/sensors/metered/MeteredDispatcherConfigurator.scala
+++ sensors/src/main/scala/nl/pragmasoft/pekko/sensors/metered/MeteredDispatcherConfigurator.scala
@@ -1,11 +1,24 @@
-package org.apache.pekko.sensors.metered
+package nl.pragmasoft.pekko.sensors.metered
 
 import com.typesafe.config.Config
-import nl.pragmasoft.pekko.sensors.metered.{MeteredDispatcher, MeteredDispatcherSettings, MeteredDispatcherSetup}
 import org.apache.pekko.dispatch.{DispatcherPrerequisites, MessageDispatcher, MessageDispatcherConfigurator}
 
+import java.util.concurrent.TimeUnit
+import scala.concurrent.duration.{Duration, FiniteDuration}
+
+private object MeteredDispatcherConfigurator {
+  final implicit class ConfigOps(val config: Config) extends AnyVal {
+    def getMillisDuration(path: String): FiniteDuration = getDuration(path, TimeUnit.MILLISECONDS)
+
+    def getNanosDuration(path: String): FiniteDuration = getDuration(path, TimeUnit.NANOSECONDS)
+
+    private def getDuration(path: String, unit: TimeUnit): FiniteDuration =
+      Duration(config.getDuration(path, unit), unit)
+  }
+}
+
 class MeteredDispatcherConfigurator(config: Config, prerequisites: DispatcherPrerequisites) extends MessageDispatcherConfigurator(config, prerequisites) {
-  import org.apache.pekko.util.Helpers._
+  import MeteredDispatcherConfigurator.ConfigOps
 
   private val instance: MessageDispatcher = {
     val _metrics = MeteredDispatcherSetup.setupOrThrow(prerequisites).metrics
